f57284250a602dc04408421be473ed870dd28ca2
coconan <coconan.by@gmail.com>
2023-04-13 17:21:27 +0800

    initial commit

---
+++ Agilecount/AgilecountApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgilecountApp.swift
+//  Agilecount
+//
+//  Created by brian.yang on 2023/4/13.
+//
+
+import SwiftUI
+
+@main
+struct AgilecountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+## An iPhone host beancount to protect data privacy and provide mobility
+
+- [How to embed a Python interpreter in an iOS app - presented by Łukasz Langa](https://www.youtube.com/watch?v=gznPQpZH1Yc)
